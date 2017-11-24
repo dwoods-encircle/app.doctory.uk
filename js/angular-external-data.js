15c78c0c70c227848c329d2dcb2fa7e65ebdf3f3
Darren Woods <dwoods@encircle.co.uk>
2017-11-24 03:19:54 +0000

    initial commit

---
+++ js/angular-external-data.js
@@ -0,0 +1,34 @@
+angular.module("sampleApp", [])
+  .filter('sumOfValue', function() {
+    return function(data, key) {
+      debugger;
+      if (angular.isUndefined(data) || angular.isUndefined(key))
+        return 0;
+      var sum = 0;
+
+      angular.forEach(data, function(v, k) {
+        sum = sum + parseInt(v[key]);
+      });
+      return sum;
+    }
+  }).filter('totalSumPriceQty', function() {
+    return function(data, key1, key2) {
+      if (angular.isUndefined(data) || angular.isUndefined(key1) || angular.isUndefined(key2))
+        return 0;
+
+      var sum = 0;
+      angular.forEach(data, function(v, k) {
+        sum = sum + (parseInt(v[key1]) * parseInt(v[key2]));
+      });
+      return sum;
+    }
+  }).controller("TimetableCtrl", function($scope, $http) {
+    $http.get('data/standard.json')
+	.success(function(data) { 
+		$scope.sessions = data.sessions;
+	})
+	.error(function(data) { console.log('error') });
+  });
+
+
+
